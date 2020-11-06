d9ad35fa6e2199cf2b2f4f269c3468681132a644
Karel Krýda <59953812+karelkryda@users.noreply.github.com>
2020-11-06 21:49:36 +0100

    Create Connect.js

---
+++ commands/connect.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: "connect",
+    aliases: ["join"],
+    description: "Connect to voice channel",
+    async execute(message, prefix, serverQueue, client) {
+        const voiceChannel = message.member.voice.channel;
+
+        if (!serverQueue) {
+            const player = client.manager.create({
+                selfDeafen: true,
+                guild: message.guild.id,
+                serverName: message.guild.name,
+                voiceChannel: message.member.voice.channel.id,
+                textChannel: message.channel.id,
+            });
+
+            player.connect();
+            
+            message.channel.send("Joined");
+        }
+    }
+};
