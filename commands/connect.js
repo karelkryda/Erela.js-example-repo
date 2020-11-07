ff933f1bbe32e55f4d2007db481303d648b03e25
Karel Krýda <59953812+karelkryda@users.noreply.github.com>
2020-11-07 21:53:48 +0100

    Added command requirements

--- commands/connect.js
+++ commands/connect.js
@@ -1,19 +1,24 @@
-module.exports = {
+module.exports.run = async (message, prefix, serverQueue, client) => {
+    if (!serverQueue) {
+        const player = client.manager.create({
+            selfDeafen: true,
+            guild: message.guild.id,
+            serverName: message.guild.name,
+            voiceChannel: message.member.voice.channel.id,
+            textChannel: message.channel.id,
+        });
+        player.connect();
+        message.channel.send("Joined");
+    }
+}
+
+module.exports.help = {
     name: "connect",
     aliases: ["join"],
-    description: "Connect to voice channel",
-    async execute(message, prefix, serverQueue, client) {
-        const voiceChannel = message.member.voice.channel;
-        if (!serverQueue) {
-            const player = client.manager.create({
-                selfDeafen: true,
-                guild: message.guild.id,
-                serverName: message.guild.name,
-                voiceChannel: message.member.voice.channel.id,
-                textChannel: message.channel.id,
-            });
-            player.connect();            
-            message.channel.send("Joined");
-        }
-    }
-};
+    description: "Connect to voice channel"
+}
+
+module.exports.requirements = {
+    ownerOnly: false,
+    userConnection: true
+}
