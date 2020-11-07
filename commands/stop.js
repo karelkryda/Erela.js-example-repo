eb82062dc9d8dc82fc202373820e185104322b72
Karel Krýda <59953812+karelkryda@users.noreply.github.com>
2020-11-07 22:07:50 +0100

    Added command requirements

--- commands/stop.js
+++ commands/stop.js
@@ -1,12 +1,17 @@
-module.exports = {
+module.exports.run = (message, prefix, serverQueue, client) => {
+    if (!serverQueue || !serverQueue.queue.totalSize) return message.channel.send("nothing is playing");
+    serverQueue.queue.clear();
+    serverQueue.stop();
+    message.channel.send("Stopped...");
+}
+
+module.exports.help = {
     name: "stop",
     aliases: ["clear"],
-    description: "Stop the currently playing song and clear the queue",
-    execute(message, prefix, serverQueue, client) {
-        if (serverQueue.queue.totalSize) {
-            serverQueue.queue.clear();
-            serverQueue.stop();
-            message.channel.send("Stopped...");
-        }
-    }
-};
+    description: "Stop the currently playing song and clear the queue"
+}
+
+module.exports.requirements = {
+    ownerOnly: false,
+    userConnection: true
+}
