acf019e56b3b2b86669c7d5a038f841674bdbce2
Karel Krýda <59953812+karelkryda@users.noreply.github.com>
2020-11-07 11:54:47 +0100

    Create remove.js

---
+++ commands/remove.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: "remove",
+    aliases: ["rm"],
+    description: "Removes the specified track from the queue.",
+    execute(message, prefix, serverQueue) {
+        if (!serverQueue.queue.totalSize) return message.channel.send("no songs in the queue");
+
+        var splittedMessage = message.content.split(' ').slice(1)[0];
+
+        if (splittedMessage >= serverQueue.queue.totalSize) return message.channel.send(`There aren't that many songs`);
+        var title = serverQueue.queue[splittedMessage - 1].title;
+        serverQueue.queue.remove(splittedMessage - 1)
+        return message.channel.send(`\`${title}\` was successfully removed`);
+    }
+};
