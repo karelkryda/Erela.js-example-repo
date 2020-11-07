4eb0f714381ff91e00a50ae70c2fde8cd008aa56
Karel Krýda <59953812+karelkryda@users.noreply.github.com>
2020-11-07 22:01:55 +0100

    Added command requirements

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,14 @@
-module.exports = {
+module.exports.run = (message, prefix, serverQueue, client) => {
+    message.channel.send(`:ping_pong: **Pong!**`);
+}
+
+module.exports.help = {
     name: "ping",
     aliases: [],
-    description: "Ping command",
-    execute(message, prefix, serverQueue, client) {
-        message.channel.send(`:ping_pong: **Pong!**`);
-    }
-};
+    description: "Ping command"
+}
+
+module.exports.requirements = {
+    ownerOnly: false,
+    userConnection: false
+}
