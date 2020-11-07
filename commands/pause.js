012d0587896acbf75fbdd7da70ec19cae3e0005c
Karel Krýda <59953812+karelkryda@users.noreply.github.com>
2020-11-07 22:00:29 +0100

    Added command requirements

--- commands/pause.js
+++ commands/pause.js
@@ -1,10 +1,18 @@
-module.exports = {
+module.exports.run = (message, prefix, serverQueue, client) => {
+    if (!serverQueue || !serverQueue.queue.totalSize) return message.channel.send("nothing is playing");
+    if (serverQueue.paused) return message.channel.send("Music is already paused");
+    serverQueue.pause(true); //false for resume
+    return message.channel.send("Paused");
+}
+
+
+module.exports.help = {
     name: "pause",
     aliases: [],
-    description: "Pause the player",
-    execute(message, prefix, serverQueue, client) {
-        if (serverQueue.paused) return message.channel.send("Music is already paused");
-        serverQueue.pause(true); //false for resume
-        return message.channel.send("Paused");
-    }
-};
+    description: "Pause the player"
+}
+
+module.exports.requirements = {
+    ownerOnly: false,
+    userConnection: true
+}
